7db548933a44075d3243addddcef55e7c7e3fbb0
nowl01 <itsabdennacerama@gmail.com>
2022-06-12 05:55:47 +0100

    feat: responsivity for landing page

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -2,8 +2,8 @@ import { Button } from '@mui/material'
 import type { NextPage } from 'next'
 import classes from "../styles/LandingPage.module.css"
 import Image from 'next/image'
-import mypic from "../public/42.jpg";
-import myguy from "../public/player1.svg"
+import pic42 from "../public/42.jpg";
+import playerImg from "../public/player1.svg"
 import logoImg from "../public/ponglogo.svg"
 import LoginIcon from '@mui/icons-material/Login';
 // import Head from 'next/head'
@@ -41,23 +41,28 @@ import LoginIcon from '@mui/icons-material/Login';
 
 const Home: NextPage = () => {
   return (
-    <div className={classes.test}>
+    <div>
 		<div className={classes.container}>
 			<div className={classes.container__left}>
-				<div className={classes.div__logo}>
-					<Image className={classes.img__logo} src={logoImg} alt="this is the logo" height={60} width={60} />
+				<div className={classes.container__up}>
+					<div className={classes.div__logo}>
+						<Image className={classes.img__logo} src={logoImg} alt="this is the logo" height="80em" width="80em" />
+					</div>
+					<div className={classes.login__button_1}>
+						<Button variant="contained" size="small" startIcon={<LoginIcon />} style={{backgroundColor:"#ff4842 ", borderRadius: "10px", width: "35%", fontSize: "0.7rem"}}>Login</Button>
+					</div>
 				</div>
 				<div className={classes.div__player}>
-					<Image className={classes.img__player} src={myguy} alt="a ping pong player"  layout="fill"/>
+					<Image className={classes.img__player} src={playerImg} alt="a ping pong player"  layout="fill"/>
 				</div>
 			</div>
 			<div className={classes.container__right}>
 				<div className={classes.login}>
 					<div className={classes.login__image}>
-						<Image className={classes.image__42} src={mypic} alt="A 42 image " objectFit="cover" layout='fill' />
+						<Image className={classes.image__42} src={pic42} alt="A 42 image " objectFit="cover" layout='fill' />
 					</div>
 					<div className={classes.login__button}>
-						<Button variant="contained" size="medium" startIcon={<LoginIcon />} style={{backgroundColor:"#ff4842 ", borderRadius: "12px", width: "35%"}}>Login</Button>
+						<Button variant="contained" size="medium" startIcon={<LoginIcon />} style={{backgroundColor:"#ff4842 ", borderRadius: "12px", width: "35%", fontSize: "1vw"}}>Login</Button>
 					</div>
 				</div>
 			</div>
