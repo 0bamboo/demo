622acd4eed4851dd8bec7f8332de52b43693efac
nowl01 <itsabdennacerama@gmail.com>
2022-06-30 18:28:12 +0100

    feat: remove some nav components

--- next-app/components/SideBar.tsx
+++ next-app/components/SideBar.tsx
@@ -6,15 +6,9 @@ const SideBar = () => {
 			<div className="middle-sidebar">
 				<ul className="sidebar-list">
 					<li className="sidebar-list-item active">
-						<a href="#" className="sidebar-link">
-						<svg xmlns="http://www.w3.org/2000/svg"  className="sidebar-icon" viewBox="0 0 16 16"><path fillRule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/><path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/></svg>
-						<div className="hidden-sidebar active">Home</div>
-						</a>
-					</li>
-					<li className="sidebar-list-item">
-						<a href="#" className="sidebar-link">
+						<a href="#" className="sidebar-link ">
 						<svg xmlns="http://www.w3.org/2000/svg"  className="sidebar-icon" viewBox="0 0 16 16"><path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>
-						<div className="hidden-sidebar">Profile</div>
+						<div className="hidden-sidebar active">Profile</div>
 						</a>
 					</li>
 					<li className="sidebar-list-item">
@@ -27,12 +21,6 @@ const SideBar = () => {
 						<a href="#" className="sidebar-link">
 						<svg xmlns="http://www.w3.org/2000/svg" className="sidebar-icon" viewBox="0 0 16 16"><path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/><path fillRule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/><path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/></svg>
 						<div className="hidden-sidebar">Users</div>
-						</a>
-					</li>
-					<li className="sidebar-list-item">
-						<a href="#" className="sidebar-link">
-						<svg xmlns="http://www.w3.org/2000/svg" className="sidebar-icon" viewBox="0 0 16 16"><path d="M0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zM2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3zm2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1z"/></svg>
-						<div className="hidden-sidebar">Channels</div>
 						</a>
 					</li>
 					<li className="sidebar-list-item">
